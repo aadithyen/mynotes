4bbce6baa1f7c2101cf8ec410c5100a6ef7080d2
Aadithye N <aadithyen@gmail.com>
2023-01-09 17:52:23 +0530

    Added gdsc logo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import { Box, Button, Flex, Heading } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { FaGoogle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../AuthContext";
+import gdscLogo from "../assets/gdsc logo.webp";
 
 const Login = () => {
 	const navigate = useNavigate();
@@ -30,13 +31,19 @@ const Login = () => {
 				justifyContent="center"
 				alignItems="center"
 			>
-				<Heading as="h1" size="4xl">
+				<Heading as="h1" size="4xl" mt="auto">
 					my notes
 				</Heading>
 
-				<Button leftIcon={<FaGoogle />} mt="12" onClick={handleGoogleSignIn}>
+				<Button
+					leftIcon={<FaGoogle />}
+					mt="12"
+					mb="auto"
+					onClick={handleGoogleSignIn}
+				>
 					Sign in with Google
 				</Button>
+				<Image src={gdscLogo} w="2xs" />
 			</Flex>
 		</Box>
 	);
